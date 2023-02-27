c99f98f98df2e7a5ff3a326fdf609455cd5d4fe9
Pedro Vicente <2210716@my.ipleiria.pt>
2023-02-27 20:09:15 +0000

    oop, created add remove and edit methods

--- gui.py
+++ gui.py
@@ -1,45 +1,58 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.geometry("500x500")
+class App:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("500x500")
 
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=1)
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=1)
 
-# Create the label and listbox in the first column
-stock_label = tk.Label(root, text="Stock")
-stock_label.grid(row=0, column=0, sticky="w")
+        # Create the label and listbox in the first column
+        self.stock_label = tk.Label(self.root, text="self.stock")
+        self.stock_label.grid(row=0, column=0, sticky="w")
 
-stock_listbox = tk.Listbox(root)
-stock_listbox.grid(row=1, column=0, sticky="nsew")
-scrollbar = tk.Scrollbar(root, command=stock_listbox.yview)
-scrollbar.grid(row=1, column=0, sticky="nse")
+        self.stock_listbox = tk.Listbox(self.root)
+        self.stock_listbox.grid(row=1, column=0, sticky="nsew")
+        self.scrollbar = tk.Scrollbar(self.root, command=self.stock_listbox.yview)
+        self.scrollbar.grid(row=1, column=0, sticky="nse")
 
-stock_listbox.config(yscrollcommand=scrollbar.set)
+        self.stock_listbox.config(yscrollcommand=self.scrollbar.set)
 
-# Create a container frame for the buttons
-button_frame = tk.Frame(root)
-button_frame.grid(row=1, column=2, padx=10, pady=10)
+        # Create a container frame for the self.buttons
+        self.button_frame = tk.Frame(self.root)
+        self.button_frame.grid(row=1, column=2, padx=10, pady=10)
 
-# Create the buttons inside the container frame
-button1 = tk.Button(button_frame, text="Button 1")
-button2 = tk.Button(button_frame, text="Button 2")
-button3 = tk.Button(button_frame, text="Button 3")
-button4 = tk.Button(button_frame, text="Button 4")
+        # Create the self.buttons inside the container frame
+        self.button1 = tk.Button(self.button_frame, text="self.button 1")
+        self.button2 = tk.Button(self.button_frame, text="self.button 2")
+        self.button3 = tk.Button(self.button_frame, text="self.button 3")
+        self.button4 = tk.Button(self.button_frame, text="self.button 4")
 
-# Add the buttons to the second column with 90% width
-button1.grid(row=0, column=1, pady=10, sticky="ew", padx=5)
-button2.grid(row=1, column=1, pady=10, sticky="ew", padx=5)
-button3.grid(row=2, column=1, pady=10, sticky="ew", padx=5)
-button4.grid(row=3, column=1, pady=10, sticky="ew", padx=5)
+        # Add the self.buttons to the second column with 90% width
+        self.button1.grid(row=0, column=1, pady=10, sticky="ew", padx=5)
+        self.button2.grid(row=1, column=1, pady=10, sticky="ew", padx=5)
+        self.button3.grid(row=2, column=1, pady=10, sticky="ew", padx=5)
+        self.button4.grid(row=3, column=1, pady=10, sticky="ew", padx=5)
 
-# Match the height of the container frame to the listbox
-button_frame.configure(height=stock_listbox.winfo_height())
+        # Match the height of the container frame to the listbox
+        self.button_frame.configure(height=self.stock_listbox.winfo_height())
 
-# Make the first column expand vertically to fill the space
-root.rowconfigure(1, weight=1)
+        # Make the first column expand vertically to fill the space
+        self.root.rowconfigure(1, weight=1)
+        self.names = []
 
-for x in range(100):
-    stock_listbox.insert(tk.END, "This is line number " + str(x))
+        self.root.mainloop()
 
-root.mainloop()
+    def Add(self, name: str, quantity: int):
+        self.stock_listbox.insert(tk.END, f"{name}\t{quantity}")
+        self.names.append(name)
+    
+    def Remove(self, name: str):
+        self.stock_listbox.delete(self.names.index(name))
+
+    def edit(self, name: str, quantity: int):
+        index = self.names.index(name)
+        self.stock_listbox.delete(index)
+        self.stock_listbox.insert(index, f"{name}\t{quantity}")
+App()
